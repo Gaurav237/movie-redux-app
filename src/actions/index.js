e4128b2c126b92fece898e283dcd9d37dc5d1c58
Gaurav237 <gktiwary237@gmail.com>
2023-06-21 02:44:35 +0530

    replaced actions with action creators & action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
+// action types
+export const ADD_MOVIES = 'ADD_MOVIES';
 
-export const AddMovies = () => {
-    return {
-      type: 'ADD_MOVIES',
-      movies: [m1, m2, m3]
-    };
-};
+
+// action creators
+export function addMovies (movies) {
+  return {
+    type: ADD_MOVIES,
+    movies: movies
+  }
+}
